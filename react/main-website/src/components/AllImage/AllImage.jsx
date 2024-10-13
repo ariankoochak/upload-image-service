803700320d3d897c4ddad9702a32c686a3c62b53
Arian <arian.koochak@protonmail.com>
2024-10-13 15:05:49 +0330

    add get image and show image

--- react/main-website/src/components/AllImage/AllImage.jsx
+++ react/main-website/src/components/AllImage/AllImage.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+
 
 export default function AllImage() {
+    const [images,setImages] = useState([]);
+    useEffect(()=>{
+        const api = "http://localhost:3000/get-data/";
+        axios({
+            withCredentials: true,
+            method: "get",
+            url: api,
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    setImages(response.data)
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },[])
+
+    const renderImages = ()=>{
+        return images.map((imageName)=>{
+            return <div key={imageName}>
+                <img src={`http://localhost:3000/upload/${imageName}`} alt=""/>
+            </div>
+        })
+    }
   return (
-    <div>AllImage</div>
+    <div>{renderImages()}</div>
   )
 }
