c57f30a87327294cf31a8f9267b3641c184b6e87
Arian <arian.koochak@protonmail.com>
2024-10-13 12:46:23 +0330

    add catching errors

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const allRoutes = require("./src/modules/router/routes");
 app.use(express.static("storage"));
 
 app.use(allRoutes);
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    return res.json({
+        error: {
+            message: err.message || "internal server error",
+        },
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Upload Service listening on port ${PORT}`);
