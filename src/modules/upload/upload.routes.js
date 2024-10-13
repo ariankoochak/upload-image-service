0aba15de05351aaf8cf56c601fb35daab210d206
Arian <arian.koochak@protonmail.com>
2024-10-13 12:56:34 +0330

    add controller to route

--- src/modules/upload/upload.routes.js
+++ src/modules/upload/upload.routes.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
 const router = Router();
-const {uploadFile} = require('../../config/multer.config')
+const {uploadFile} = require('../../config/multer.config');
+const uploadController = require("./upload.controller");
 
-router.post("/", uploadFile.single("image"));
+router.post("/", uploadFile.single("image"),uploadController.uploadImage);
 
 module.exports = router;
