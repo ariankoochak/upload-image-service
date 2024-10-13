2f73b829030f05b2525ab3bf59d44c08500b685d
Arian <arian.koochak@protonmail.com>
2024-10-13 13:42:26 +0330

    add main website to route

--- src/modules/router/routes.js
+++ src/modules/router/routes.js
@@ -3,7 +3,9 @@ const router = Router();
 
 const uploadRouter = require('../upload/upload.routes')
 const getDataRouter = require('../getData/getData.routes')
+const websiteRouter = require('../website/website.route')
 
+router.use("/", websiteRouter);
 router.use("/upload", uploadRouter);
 router.use("/get-data",getDataRouter );
 
