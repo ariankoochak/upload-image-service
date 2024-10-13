203e818d1f4374b163bf26ada588b8d8863100c5
Arian <arian.koochak@protonmail.com>
2024-10-13 13:20:21 +0330

    add getAllImage controller

--- src/modules/getData/getData.controller.js
+++ src/modules/getData/getData.controller.js
@@ -1,7 +1,12 @@
+const fs = require('fs')
+const path = require('path')
+
 class getDataController{
     getAllImagesName(req,res,next){
         try {
-            res.send('hameye aksa')
+            const directoryPath = path.join(process.cwd(),'storage', 'upload');
+            const files = fs.readdirSync(directoryPath)
+            res.send(files)    
         } catch (err) {
             next(err)
         }
