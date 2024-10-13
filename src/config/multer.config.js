ec8df6b4593b55373566a8c54e2ecd0b6a6ad0fb
Arian <arian.koochak@protonmail.com>
2024-10-13 13:05:24 +0330

    update upload file route

--- src/config/multer.config.js
+++ src/config/multer.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, process.cwd() + "/storage");
+        cb(null, process.cwd() + "/storage/upload");
     },
     filename: async (req, file, cb) => {
         const ext = path.extname(file.originalname);
